8f5cdd7d415d99d1946e2997d2ee21f7351c29de
Max Mehl <mail@mehl.mx>
2025-10-09 23:37:55 +0200

    feat: elaborate email sending, but it somehow broke and the logic cannot decide between broadcast and mlist

--- castmail2list/mailer.py
+++ castmail2list/mailer.py
@@ -6,6 +6,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import formatdate, make_msgid
 
+from imap_tools.utils import EmailAddress
+
 
 class Mail:  # pylint: disable=too-many-instance-attributes,too-few-public-methods
     """Class for an email with specific template and subject this app will send"""
@@ -26,21 +28,60 @@ class Mail:  # pylint: disable=too-many-instance-attributes,too-few-public-metho
         self.smtp_starttls: bool = smtp_starttls
         self.smtp_from: str = smtp_from
 
-    def send_email(self, subject: str, message: str, recipient: str) -> None:
+    def send_email(  # pylint: disable=too-many-arguments,too-many-positional-arguments
+        self,
+        list_address: str,
+        list_name: str,
+        from_addr: EmailAddress,
+        subject: str,
+        recipient: str,
+        to_header: tuple[str, ...],
+        cc_header: tuple[str, ...],
+        date_header: str,
+        msg_id: str,
+        text_message: str = "",
+        html_message: str = "",
+        attachments: None | list[tuple[str, bytes, str]] = None,
+    ) -> None:
         """
         Sends an email using a Jinja2 template.
         """
 
-        # Create the email message
-        msg = MIMEMultipart()
-        msg["From"] = self.smtp_from
-        msg["To"] = recipient
-        msg["Subject"] = subject
-        msg["Message-ID"] = make_msgid(idstring="castmail2list", domain="localhost")
-        msg["Date"] = formatdate(localtime=True)
+        # Deal with recipient as possible To of original message
+        if recipient in to_header:
+            # TODO: Decide what to do if recipient is also in To header
+            pass
+        if list_address in to_header or list_address in cc_header:
+            # Remove list address from To and CC headers to avoid confusion
+            # TODO: Depending on list settings as broadcast or real mailing list, this needs to be
+            # handled differently
+            to_header = tuple(addr for addr in to_header if addr != list_address)
+            cc_header = tuple(addr for addr in cc_header if addr != list_address)
 
-        # Attach the email body as HTML
-        msg.attach(MIMEText(message, "html"))
+        # Model new From field
+        from_header = f"{from_addr.name} via {list_name} <{list_address}>"
+
+        # Create the email message wth all necessary headers
+        msg = MIMEMultipart()
+        msg["From"] = from_header
+        msg["To"] = ", ".join(to_header) if to_header else recipient
+        if cc_header:
+            msg["Cc"] = ", ".join(cc_header)
+        msg["Subject"] = subject
+        msg["Message-ID"] = msg_id
+        msg["Date"] = date_header or formatdate(localtime=True)
+        # TODO: Set List-ID only for real mailing lists, not for broadcast mode
+        msg["List-Id"] = f"{list_name} <{list_address.replace('@', '.')}>"
+        msg["X-Mailer"] = "CastMail2List"
+
+        # Attach the email body in Plain and HTML
+        if html_message:
+            msg.attach(MIMEText(html_message, "html"))
+        if text_message:
+            msg.attach(MIMEText(text_message, "plain"))
+        if not html_message and not text_message:
+            logging.warning("No HTML or Plaintext content provided for email to %s", recipient)
+
         logging.debug("Email content: \n%s", msg.as_string())
 
         try:
