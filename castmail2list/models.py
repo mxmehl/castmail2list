8bae3a7431ae3d6d20b7c26391b88cb0cd5332a4
Max Mehl <max.mehl@deutschebahn.com>
2025-10-16 16:56:01 +0200

    chore: typing, pylint, minimal refactor

--- castmail2list/models.py
+++ castmail2list/models.py
@@ -15,9 +15,9 @@ else:
 class List(Model):  # pylint: disable=too-few-public-methods
     """A mailing list"""
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True)
-    address = db.Column(db.String)
+    id: int = db.Column(db.Integer, primary_key=True)
+    name: str = db.Column(db.String, unique=True)
+    address: str = db.Column(db.String)
     subscribers = db.relationship(
         "Subscriber", backref="list", lazy=True, cascade="all, delete-orphan"
     )
@@ -25,12 +25,12 @@ class List(Model):  # pylint: disable=too-few-public-methods
     messages = db.relationship("Message", backref="list", lazy=True, cascade="all, delete-orphan")
 
     # Technical settings per list
-    mode = db.Column(db.String)
-    imap_host = db.Column(db.String)
-    imap_port = db.Column(db.String)
-    imap_user = db.Column(db.String)
-    imap_pass = db.Column(db.String)
-    from_addr = db.Column(db.String)
+    mode: str = db.Column(db.String)
+    imap_host: str = db.Column(db.String)
+    imap_port: str | int = db.Column(db.String)
+    imap_user: str = db.Column(db.String)
+    imap_pass: str = db.Column(db.String)
+    from_addr: str = db.Column(db.String)
     allowed_senders: str = db.Column(db.Text)  # Comma-separated list of allowed sender emails
     only_subscribers_send: bool = db.Column(
         db.Boolean, default=False
@@ -41,20 +41,20 @@ class Subscriber(Model):  # pylint: disable=too-few-public-methods
     """A subscriber to a mailing list"""
 
     id = db.Column(db.Integer, primary_key=True)
-    list_id = db.Column(db.Integer, db.ForeignKey("list.id"), nullable=False)
-    name = db.Column(db.String)
-    email = db.Column(db.String, nullable=False)
-    comment = db.Column(db.String)
+    list_id: int = db.Column(db.Integer, db.ForeignKey("list.id"), nullable=False)
+    name: str = db.Column(db.String)
+    email: str = db.Column(db.String, nullable=False)
+    comment: str = db.Column(db.String)
 
 
 class Message(Model):  # pylint: disable=too-few-public-methods
     """A message sent to a mailing list"""
 
-    id = db.Column(db.Integer, primary_key=True)
-    list_id = db.Column(db.Integer, db.ForeignKey("list.id"))
-    message_id = db.Column(db.String, unique=True)
-    subject = db.Column(db.String)
-    from_addr = db.Column(db.String)
-    headers = db.Column(db.Text)
-    raw = db.Column(db.Text)  # for now, store full RFC822 text
+    id: int = db.Column(db.Integer, primary_key=True)
+    list_id: int = db.Column(db.Integer, db.ForeignKey("list.id"))
+    message_id: str = db.Column(db.String, unique=True)
+    subject: str = db.Column(db.String)
+    from_addr: str = db.Column(db.String)
+    headers: dict[str, tuple[str, ...]] = db.Column(db.Text)
+    raw: str = db.Column(db.Text)  # for now, store full RFC822 text
     received_at = db.Column(db.DateTime, default=datetime.utcnow)
