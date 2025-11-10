7d0564abee9dce55b1353c5b3d961152e2e0600c
Max Mehl <max.mehl@deutschebahn.com>
2025-11-10 22:49:16 +0100

    feat: message detail page

--- castmail2list/views/messages.py
+++ castmail2list/views/messages.py
@@ -1,6 +1,6 @@
 """Messages blueprint for CastMail2List application"""
 
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, render_template
 from flask_login import login_required
 
 from ..models import Message
@@ -11,6 +11,15 @@ messages = Blueprint("messages", __name__, url_prefix="/messages")
 @messages.route("/")
 @login_required
 def show_all() -> str:
-    """Show recent messages"""
-    msgs: list[Message] = Message.query.order_by(Message.received_at.desc()).limit(20).all()
+    """Show all messages"""
+    msgs: list[Message] = Message.query.order_by(Message.received_at.desc()).all()
     return render_template("messages/index.html", messages=msgs)
+
+@messages.route("/<int:message_id>")
+@login_required
+def show(message_id: int) -> str:
+    """Show a specific message"""
+    msg: Message | None = Message.query.get(message_id)
+    if not msg:
+        flash("Message not found", "error")
+    return render_template("messages/detail.html", message=msg)
