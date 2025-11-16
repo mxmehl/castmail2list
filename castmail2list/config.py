99e4f795f6a248099d477ba4a4c3f03ff6ffaa7a
Max Mehl <mail@mehl.mx>
2025-11-16 22:59:00 +0100

    chore: update config fields

--- castmail2list/config.py
+++ castmail2list/config.py
@@ -15,15 +15,15 @@ class Config:  # pylint: disable=too-few-public-methods
 
     # IMAP settings and defaults (used as defaults for new lists)
     IMAP_DEFAULT_HOST = os.getenv("IMAP_DEFAULT_HOST", "***REMOVED***")
+    IMAP_DEFAULT_DOMAIN = os.getenv("IMAP_DEFAULT_DOMAIN", "***REMOVED***")
     IMAP_DEFAULT_PORT = os.getenv("IMAP_DEFAULT_PORT", "993")
     IMAP_DEFAULT_PASS = os.getenv("IMAP_DEFAULT_PASS", "testtest123")
+
+    # IMAP folder names
     IMAP_FOLDER_INBOX = os.getenv("IMAP_FOLDER_INBOX", "INBOX")
     IMAP_FOLDER_PROCESSED = os.getenv("IMAP_FOLDER_PROCESSED", "Processed")
     IMAP_FOLDER_SENT = os.getenv("IMAP_FOLDER_SENT", "Sent")
     IMAP_FOLDER_BOUNCES = os.getenv("IMAP_FOLDER_BOUNCES", "Bounces")
-
-    # Default "from" address for new lists
-    IMAP_LIST_FROM = os.getenv("IMAP_LIST_FROM", "noreply@***REMOVED***")
 
     # SMTP settings (defaults for new lists)
     SMTP_HOST = os.getenv("SMTP_HOST", "***REMOVED***")
@@ -32,6 +32,4 @@ class Config:  # pylint: disable=too-few-public-methods
     SMTP_PASS = os.getenv("SMTP_PASS", "testtest123")
     SMTP_STARTTLS = os.getenv("SMTP_STARTTLS", "true").lower() in ("true", "1", "yes")
 
-    POLL_INTERVAL = int(os.getenv("POLL_INTERVAL", "60"))  # seconds
-
-    # Removed: IMAP_LIST_USER (single-list user), as lists are now dynamic
+    POLL_INTERVAL_SECONDS = int(os.getenv("POLL_INTERVAL_SECONDS", "10"))
